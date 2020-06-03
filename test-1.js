17dc0da865519f51ab617b412f20cc8c3944390e
StepanKobrii18321832 <esestepan2000@gmail.com>
2020-06-03 11:57:23 +0300

    gitignore and eslint

---
+++ test-1.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  host: '127.0.0.1',
+  port: 3000,
+  database: 'application',
+  user: 'stepll',
+  password: 'stepan2000',
+});
+
+const fields = ['schemaname', 'tablename', 'tableowner'].join(', ');
+const sql = `SELECT ${fields} FROM pg_tables WHERE tableowner = $1`;
+pool.query(sql, ['stepll'], (err, res) => {
+  if (err) {
+    throw err;
+  }
+  console.dir({ res });
+  console.table(res.fields);
+  console.table(res.rows);
+  pool.end();
+});
